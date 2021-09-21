860dd9c7c43d667256fbbcb7304f4d39f3d0c794
CyberfuzZ <keepthelight@icloud.com>
2021-09-21 12:28:39 +0300

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,25 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        System.out.println(first.getName() + " " + first.getState());
+        first.start();
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " " + first.getState());
+        }
+        System.out.println(first.getName() + " " + first.getState());
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        System.out.println(second.getName() + " " + second.getState());
+        second.start();
+        while (second.getState() != Thread.State.TERMINATED) {
+            System.out.println(second.getName() + " " + second.getState());
+        }
+        System.out.println(second.getName() + " " + first.getState());
+        System.out.println(Thread.currentThread().getName() + " Work is over");
+    }
+}
