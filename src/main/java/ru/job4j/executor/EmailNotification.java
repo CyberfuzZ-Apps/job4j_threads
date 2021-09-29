12ca8f339c60fe6e5a029a88d4d0f7b335df6022
CyberfuzZ <keepthelight@icloud.com>
2021-09-29 12:10:18 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/executor/EmailNotification.java
@@ -0,0 +1,27 @@
+package ru.job4j.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        pool.execute(() -> {
+            String subject = String.format(
+                    "Notification %s to email %s", user.getUsername(), user.getEmail());
+            String body = String.format("Add a new event to %s", user.getUsername());
+            String email = user.getEmail();
+            send(subject, body, email);
+        });
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+}
