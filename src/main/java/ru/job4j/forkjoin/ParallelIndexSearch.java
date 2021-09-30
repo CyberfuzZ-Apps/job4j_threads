2e794d5196748a182d6e3ab62b869d123161ed0f
CyberfuzZ <keepthelight@icloud.com>
2021-09-30 11:12:52 +0300

    3. ForkJoinPool [#315067 #210129]

--- src/main/java/ru/job4j/forkjoin/ParallelIndexSearch.java
+++ src/main/java/ru/job4j/forkjoin/ParallelIndexSearch.java
@@ -3,12 +3,26 @@ package ru.job4j.forkjoin;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
+/**
+ * Класс реализует параллельный поиск индекса объекта в массиве.
+ *
+ * @param <T> - любой тип.
+ */
 public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
     private final T[] array;
     private final T object;
     private final int start;
     private final int end;
 
+    /**
+     * Конструктор принимает исходный массив, объект для поиска и
+     * диапазон поиска - начальный и конечный индекс массива.
+     *
+     * @param array - массив, в котором будет осуществляться поиск.
+     * @param object - объект, индекс которого надо найти.
+     * @param start - стартовый индекс поиска.
+     * @param end - конечный индекс поиска.
+     */
     public ParallelIndexSearch(T[] array, T object, int start, int end) {
         this.array = array;
         this.object = object;
@@ -20,7 +34,7 @@ public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
     protected Integer compute() {
         int mid = (end + start) / 2;
         if (end - start <= 10) {
-            return indexOf(array, object, start, end);
+            return indexOf();
         }
         ParallelIndexSearch<T> pLeft = new ParallelIndexSearch<>(array, object, start, mid);
         ParallelIndexSearch<T> pRight = new ParallelIndexSearch<>(array, object, mid, end);
@@ -31,8 +45,8 @@ public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
         return Math.max(rslL, rslR);
     }
 
-    private int indexOf(T[] array, T object, int start, int end) {
-        for (int i = start; i < end; i++) {
+    private int indexOf() {
+        for (int i = start; i <= end; i++) {
             if (object.equals(array[i])) {
                 return i;
             }
@@ -40,9 +54,17 @@ public class ParallelIndexSearch<T> extends RecursiveTask<Integer> {
         return -1;
     }
 
+    /**
+     * Метод ищет индекс объекта в массиве. Если длина массива больше 10,
+     * то поиск осуществляется параллельно (в несколько потоков, зависит от
+     * количества физических процессоров). Возвращает индекс объекта в массиве или
+     * -1 если объект не найден.
+     *
+     * @return - индекс объекта в массиве, либо -1 если объект не найден.
+     */
     public int searchIndex() {
         if (array.length <= 10) {
-            return indexOf(array, object, start, end);
+            return indexOf();
         }
         ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
         return forkJoinPool.invoke(this);
--- src/main/test/ru/job4j/forkjoin/ParallelIndexSearchTest.java
+++ src/main/test/ru/job4j/forkjoin/ParallelIndexSearchTest.java
@@ -14,7 +14,7 @@ public class ParallelIndexSearchTest {
         for (int i = 0; i < array.length; i++) {
             array[i] = "str " + i;
         }
-        ParallelIndexSearch<String> p = new ParallelIndexSearch<>(array, obj, 0, array.length);
+        ParallelIndexSearch<String> p = new ParallelIndexSearch<>(array, obj, 0, array.length - 1);
         assertThat(p.searchIndex(), is(700));
     }
 
@@ -25,8 +25,41 @@ public class ParallelIndexSearchTest {
         for (int i = 0; i < array.length; i++) {
             array[i] = i;
         }
-        ParallelIndexSearch<Integer> p = new ParallelIndexSearch<>(array, obj, 0, array.length);
+        ParallelIndexSearch<Integer> p = new ParallelIndexSearch<>(array, obj, 0, array.length - 1);
         assertThat(p.searchIndex(), is(700));
+    }
+
+    @Test
+    public void whenSearchInteger2() {
+        Integer[] array = new Integer[1_000];
+        Integer obj = 999;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        ParallelIndexSearch<Integer> p = new ParallelIndexSearch<>(array, obj, 0, array.length - 1);
+        assertThat(p.searchIndex(), is(999));
+    }
+
+    @Test
+    public void whenSearchInteger3() {
+        Integer[] array = new Integer[1_000];
+        Integer obj = 1;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        ParallelIndexSearch<Integer> p = new ParallelIndexSearch<>(array, obj, 0, array.length - 1);
+        assertThat(p.searchIndex(), is(1));
+    }
+
+    @Test
+    public void whenIntegerNotFound() {
+        Integer[] array = new Integer[1_000];
+        Integer obj = 1_000_000;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        ParallelIndexSearch<Integer> p = new ParallelIndexSearch<>(array, obj, 0, array.length - 1);
+        assertThat(p.searchIndex(), is(-1));
     }
 
 }
